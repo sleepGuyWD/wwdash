66a5c72ab211ddc47b61c880166d0d2118ea7868
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2024-08-18 21:30:27 -0700

    components and first page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+
+function Footer() {
+  return(
+    <section clasName="foot">
+      <section className="footerTop">
+        <ul>
+          <li>
+            <span>ipsum lorem</span>
+          </li>
+          <li>
+            <span>ipsum lorem</span>
+          </li>
+          <li>
+            <span>ipsum lorem</span>
+          </li>
+          <li>
+            <span>ipsum lorem</span>
+          </li>
+        </ul>
+      </section>
+      <section className="footerBottom">
+        <span>c WD 2024. All rights reserved. </span>
+      </section>
+
+    </section>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+function Header() {
+  return (
+    <nav>
+      <img src="./src/assets/Capture.PNG" alt="logo" className="logoImg"/>
+      <section className="greyArea"></section>
+    </nav>
+  )
+}
+
+export default Header
